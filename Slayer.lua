aa7bc8c75fa2cd158c953ceee747eef2424bd1cf
FJN19-dev <josealbertsouzasantos189@gmail.com>
2025-11-20 16:21:14 -0300

    Refactor game loading and settings initialization
    Removed unnecessary waiting for game load and set up environment variables for settings.

--- Slayer.lua
+++ Slayer.lua
@@ -1,10 +1,3 @@
 if game.PlaceId ~= 2753915549 and game.PlaceId ~= 4442272183 and game.PlaceId ~= 7449423635 then return end
 
-repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
-
-getgenv().Settings = {JoinTeam = true, Team = "Marine"}
-getgenv().AutoStoreFruit = true
-getgenv().Webhook = true
-getgenv().WebhookURL = ""
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FJN19-dev/Slayer/refs/heads/main/FindFruits.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FJN19-dev/Slayer/refs/heads/main/Config.lua"))()
